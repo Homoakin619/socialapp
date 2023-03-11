f111c9f77db79e3ef7484cff124a3ad978f70eaf
homoakin619 <homohaykins619@gmail.com>
2023-03-11 09:16:09 +0100

    made few changes

--- chat/views.py
+++ chat/views.py
@@ -4,6 +4,4 @@ from django.contrib.auth.models import User
 
 def chat_view(request,username):
     friend_id = User.objects.get(username=username)
-    
-
     return render(request,'chat/chat.html',{'friend_id':friend_id})
